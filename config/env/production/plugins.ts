5ccdb76922b3a4024213f49243e22c9b8d189327
Christian Albert <chrs.albert@gmail.com>
2026-01-21 21:50:32 +0100

    try: R2 provider config in production environment

---
+++ config/env/production/plugins.ts
@@ -0,0 +1,27 @@
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: "strapi-provider-cloudflare-r2",
+      providerOptions: {
+        accessKeyId: env("CF_ACCESS_KEY_ID"),
+        secretAccessKey: env("CF_ACCESS_SECRET"),
+        endpoint: env("CF_ENDPOINT"),
+        params: {
+          Bucket: env("CF_BUCKET"),
+        },
+        cloudflarePublicAccessUrl: env("CF_PUBLIC_ACCESS_URL"),
+        pool: false,
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+      breakpoints: {
+        large: 800,
+        medium: 600,
+        small: 420,
+      },
+    },
+  },
+});
