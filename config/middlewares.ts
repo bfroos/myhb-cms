8f7995e9cd6c6d4ffad6c0fffd97f003599054f3
Christian Albert <chrs.albert@gmail.com>
2026-02-03 14:27:07 +0100

    Add old cf public access URL to contentSecurityPolicy

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,41 +1,48 @@
-export default ({ env }) => [
-  "strapi::logger",
-  "strapi::errors",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "market-assets.strapi.io",
-            env("CF_PUBLIC_ACCESS_URL")
-              ? env("CF_PUBLIC_ACCESS_URL").replace(/^https?:\/\//, "")
-              : "",
-          ],
-          "media-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "market-assets.strapi.io",
-            env("CF_PUBLIC_ACCESS_URL")
-              ? env("CF_PUBLIC_ACCESS_URL").replace(/^https?:\/\//, "")
-              : "",
-          ],
-          upgradeInsecureRequests: null,
+export default ({ env }) => {
+  const toHost = (url?: string) =>
+    url ? url.replace(/^https?:\/\//, "").replace(/\/$/, "") : "";
+
+  // Current media host (new) + optional previous media host (old)
+  const mediaHosts = [
+    toHost(env("CF_PUBLIC_ACCESS_URL")),
+    toHost(env("CF_PUBLIC_ACCESS_URL_OLD")),
+  ].filter(Boolean);
+
+  return [
+    "strapi::logger",
+    "strapi::errors",
+    {
+      name: "strapi::security",
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            "connect-src": ["'self'", "https:"],
+            "img-src": [
+              "'self'",
+              "data:",
+              "blob:",
+              "market-assets.strapi.io",
+              ...mediaHosts,
+            ],
+            "media-src": [
+              "'self'",
+              "data:",
+              "blob:",
+              "market-assets.strapi.io",
+              ...mediaHosts,
+            ],
+            upgradeInsecureRequests: null,
+          },
         },
       },
     },
-  },
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
-];
+    "strapi::cors",
+    "strapi::poweredBy",
+    "strapi::query",
+    "strapi::body",
+    "strapi::session",
+    "strapi::favicon",
+    "strapi::public",
+  ];
+};
