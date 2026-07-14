591e80fd4500ac9fc3d7812fdaa969b18bec06c4
big fucking roocket <b.roos@myhb.app>
2026-07-14 13:37:23 +0100

    fix(audit-log): UID-Cast im Service

--- src/api/audit-log/services/audit-log.ts
+++ src/api/audit-log/services/audit-log.ts
@@ -4,4 +4,4 @@
 
 import { factories } from "@strapi/strapi";
 
-export default factories.createCoreService("api::audit-log.audit-log");
+export default factories.createCoreService("api::audit-log.audit-log" as any);
