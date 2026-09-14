9a7e3ac4f6947cffa5b6e60d27f91089de912c93
MH Alikhani <92454804+mhalikhani94@users.noreply.github.com>
2026-09-14 13:03:28 +0330

    feat(youtube): add optional embedCode field

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -981,11 +981,15 @@ export interface BlocksYoutubeVideo extends Struct.ComponentSchema {
     icon: 'play';
   };
   attributes: {
+    aspectRatio: Schema.Attribute.Enumeration<['16-9', '9-16', '4-3', '1-1']> &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'16-9'>;
     cardSettings: Schema.Attribute.Component<'shared.card-design', false>;
+    embedCode: Schema.Attribute.Text;
     headline: Schema.Attribute.String;
     intro: Schema.Attribute.Text;
     poster: Schema.Attribute.Media<'images'>;
-    videoUrl: Schema.Attribute.Text & Schema.Attribute.Required;
+    videoUrl: Schema.Attribute.String & Schema.Attribute.Required;
   };
 }
 
